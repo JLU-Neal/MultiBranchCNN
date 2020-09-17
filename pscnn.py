d1afc669a1c2953404e0ebc8f5522e9175dbafc5
Neal Shinoda <35791603+JLU-Neal@users.noreply.github.com>
2020-09-17 16:58:27 +0800

    Start fusing features of two branches

---
+++ pscnn.py
@@ -0,0 +1,66 @@
+import torch.nn as nn
+
+import modules.functional as F
+from modules.voxelization import Voxelization
+from modules.shared_mlp import SharedMLP
+from modules.se import SE3d
+
+from s2cnn import s2_equatorial_grid, S2Convolution, so3_equatorial_grid, SO3Convolution, so3_integrate
+from modules.dataset import ProjectFromPointsOnSphere
+__all__ = ['PSConv']
+
+
+class PSConv(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size, resolution, with_se=False, normalize=True, eps=0):
+        super().__init__()
+
+        self.features = [3, 100, 100, out_channels]
+        self.bandwidths = [64, 16, 64]
+        self.transform = ProjectFromPointsOnSphere(self.bandwidths[0])
+        assert len(self.bandwidths) == len(self.features) - 1
+
+        sequence = []
+
+        # S2 layer
+        # =====================If we could chose another gridding method=====================
+        grid = s2_equatorial_grid(max_beta=0, n_alpha=2 * self.bandwidths[0], n_beta=1)
+        sequence.append(S2Convolution(self.features[0], self.features[1], self.bandwidths[0], self.bandwidths[1], grid))
+
+        # SO3 layers
+        for l in range(1, len(self.features) - 2):
+            nfeature_in = self.features[l]
+            nfeature_out = self.features[l + 1]
+            b_in = self.bandwidths[l]
+            b_out = self.bandwidths[l + 1]
+
+            sequence.append(nn.BatchNorm3d(nfeature_in, affine=True))
+            sequence.append(nn.ReLU())
+            grid = so3_equatorial_grid(max_beta=0, max_gamma=0, n_alpha=2 * b_in, n_beta=1, n_gamma=1)
+            sequence.append(SO3Convolution(nfeature_in, nfeature_out, b_in, b_out, grid))
+
+        sequence.append(nn.BatchNorm3d(self.features[-2], affine=True))
+        sequence.append(nn.ReLU())
+
+        self.sequential = nn.Sequential(*sequence)
+
+        # Output layer
+        output_features = self.features[-2]
+        self.out_layer = nn.Linear(output_features, self.features[-1])
+
+
+
+
+        self.point_features = SharedMLP(in_channels, out_channels)
+
+    def forward(self, inputs):
+        #projectfrompointsonsphere
+        x, m, n = self.transform(inputs)
+        x = self.sequential(x)  # [batch, feature, beta, alpha, gamma]
+        x = so3_integrate(x)  # [batch, feature]
+
+        x = self.out_layer(x)
+        #wait for implementing
+        x=F.log_softmax(x, dim=1)
+        #deinterpolate
+        fused_features = x + self.point_features(inputs)
+        return fused_features
